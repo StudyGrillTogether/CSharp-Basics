e7143257f6ec56c5ca5df37ddf6d6f1b25664621
Kshitij1ghk <kshitijwk@gmail.com>
2026-01-03 16:29:44 +0530

    allow users to perform repeated calculations as they wish adn gave them choice to exit or not

--- DAY-1/Basic-Calculator/Basic-Calculator/Program.cs
+++ DAY-1/Basic-Calculator/Basic-Calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,30 +11,45 @@ namespace Basic_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter the numbers you want to do operation on");
-            Console.WriteLine("enter your first number");
-            int num1=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter your second number:");
-            int num2=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("what do you want to do \n enter 1 for addition\n enter 2 for subtraction\n enter 3 for multiplication \n enter 4 for dividing");
-            int i = Convert.ToInt32(Console.ReadLine());
-            switch (i)
+            bool choice = true;
+            while (choice)
             {
-                case 1:
-                    Console.WriteLine("the addition is " + (num1 + num2));
-                    break;
-                case 2:
-                    Console.WriteLine("the subtraction is " + (num1 - num2));
-                    break;
-                case 3:
-                    Console.WriteLine("the multiplication is " + (num1 * num2));
-                    break;
-                case 4:
-                    Console.WriteLine("the division is " + Convert.ToDouble(num1 / num2));
-                    break;
+                Console.WriteLine("enter the numbers you want to do operation on");
+                Console.WriteLine("enter your first number");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter your second number:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("what do you want to do \n enter 1 for addition\n enter 2 for subtraction\n enter 3 for multiplication \n enter 4 for dividing");
+                int i = Convert.ToInt32(Console.ReadLine());
+                
+                switch (i)
+                {
+                    case 1:
+                        Console.WriteLine("the addition is " + (num1 + num2));
+                        break;
+                    case 2:
+                        Console.WriteLine("the subtraction is " + (num1 - num2));
+                        break;
+                    case 3:
+                        Console.WriteLine("the multiplication is " + (num1 * num2));
+                        break;
+                    case 4:
+                        Console.WriteLine("the division is " + Convert.ToDouble(num1 / num2));
+                        break;
+
+                }
+                Console.WriteLine("do you want to exit the operation or not?\n yes or no type");
+                string yes_no=Console.ReadLine();
+                if (yes_no == "yes")
+                {
+                    choice= false;
+                }
+
+
 
             }
 
         }
+     
     }
 }
---
+++ DAY-2/DAY-2-Git-exercise/DAY-2-Git-exercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_2_Git_exercise
+{
+    class person
+    {
+        public string name;
+        public int age;
+        public void Introduce()
+        {
+            Console.WriteLine("my name is: " + name + " my age is " + age);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person person1=new person();
+            person1.name = "aster";
+            person1.age = 45;
+            person person2=new person();
+            person2.name = "kshitij";
+            person2.age = 21;
+            person person3 = new person();
+            person3.name = "mordret";
+            person3.age = 27;
+            person1.Introduce();
+            person2.Introduce();
+            person3.Introduce();    
+        }
+    }
+}
