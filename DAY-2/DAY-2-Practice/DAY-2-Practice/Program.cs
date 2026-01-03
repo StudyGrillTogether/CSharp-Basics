4fc44164d4ad6f4c1ca4d251b2a6148da79f0b49
Kshitij1ghk <kshitijwk@gmail.com>
2026-01-03 15:14:02 +0530

    Learned C# classes, objects, fields, methods, and multiple object creation

---
+++ DAY-2/DAY-2-Practice/DAY-2-Practice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_2_Practice
+{
+    class car //using multiple classes
+    {
+        public string color = "red";
+    }
+    class Myclass 
+        //class members
+    {
+        public string color="red";
+        public int Max_speed = 200;
+        public void fullthrotle()
+        {
+            Console.WriteLine("this car is going on a max speed");
+        }
+    }
+    // leaving the fileds blank to create multiple objects of same class
+    class car1
+    {
+        public string color;
+        public int Max_speed;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            car Myobj = new car();
+            Console.WriteLine(Myobj.color);
+
+            Myclass Newobj=new Myclass();
+            Console.WriteLine("the color of car is" + Newobj.color + "the max speed of car is:" + Newobj.Max_speed);
+            Newobj.fullthrotle();
+
+            car1 bentley=new car1();
+            car1 ford=new car1();
+            bentley.color = "red";
+            bentley.Max_speed = 200;
+            ford.color = "blue";
+            ford.Max_speed = 300;
+            Console.WriteLine("the color of bentley is :" + bentley.color);
+            Console.WriteLine("the speed of bentley is :"+ bentley.Max_speed+
+                "\nthe color of ford is"+ford.color+"\nthe speed of ford is:"+ford.Max_speed);
+
+        }
+    }
+}
