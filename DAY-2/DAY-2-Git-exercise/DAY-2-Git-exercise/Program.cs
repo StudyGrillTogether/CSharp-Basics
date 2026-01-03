88730686c87e125dd70c6aad3e47ce1c5a715f39
Kshitij1ghk <kshitijwk@gmail.com>
2026-01-03 17:14:06 +0530

    implemented the concept of inheritance in the classes and studied parent child relationship and how we can combine them

--- DAY-2/DAY-2-Git-exercise/DAY-2-Git-exercise/Program.cs
+++ DAY-2/DAY-2-Git-exercise/DAY-2-Git-exercise/Program.cs
@@ -10,27 +10,40 @@ namespace DAY_2_Git_exercise
     {
         public string name;
         public int age;
+        
+    }
+    class adress:person
+    {
+        public string street;
+        public string city;
         public void Introduce()
         {
-            Console.WriteLine("my name is: " + name + " my age is " + age);
+            Console.WriteLine("my name is: " + name + " my age is " + age+" im from street:"+street+" im form city: "+city);
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            person person1=new person();
+            adress person1 = new adress();
             person1.name = "aster";
             person1.age = 45;
-            person person2=new person();
+            person1.street = "lmar";
+            person1.city = "madrid";
+            adress person2 = new adress();
             person2.name = "kshitij";
             person2.age = 21;
-            person person3 = new person();
+            person2.street = "elppo";
+            person2.city = "barcelona";
+            adress person3 = new adress();
             person3.name = "mordret";
             person3.age = 27;
+            person3.street = "svaoi";
+            person3.city = "osaka";
             person1.Introduce();
             person2.Introduce();
-            person3.Introduce();    
+            person3.Introduce();
+
         }
     }
 }
