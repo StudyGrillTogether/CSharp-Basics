75fa1f4fc326da6783114cf8c06dae49782b97db
Kshitij1ghk <kshitijwk@gmail.com>
2026-01-04 11:48:46 +0530

    implemented te concept of composition instead of inheritance for better logical rasoning

--- DAY-2/DAY-2-Git-exercise/DAY-2-Git-exercise/Program.cs
+++ DAY-2/DAY-2-Git-exercise/DAY-2-Git-exercise/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DAY_2_Git_exercise;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,36 +12,45 @@ namespace DAY_2_Git_exercise
     {
         public string name;
         public int age;
-        
+        //person has an adaress
+        public adress adress;
+        public void Introduce()
+        {
+            Console.WriteLine("my name is: " + name + " my age is " + age + " im from street:" + adress.street + " im form city: " + adress.city);
+        }
     }
-    class adress:person
+
+}
+    class adress
     {
         public string street;
         public string city;
-        public void Introduce()
-        {
-            Console.WriteLine("my name is: " + name + " my age is " + age+" im from street:"+street+" im form city: "+city);
-        }
-    }
+        
     internal class Program
     {
         static void Main(string[] args)
         {
-            adress person1 = new adress();
+            person person1 = new person();
             person1.name = "aster";
             person1.age = 45;
-            person1.street = "lmar";
-            person1.city = "madrid";
-            adress person2 = new adress();
+            person1.adress = new adress();
+            person1.adress.street = "lmar";
+            person1.adress.city = "madrid";
+   
+            person person2 = new person();
             person2.name = "kshitij";
             person2.age = 21;
-            person2.street = "elppo";
-            person2.city = "barcelona";
-            adress person3 = new adress();
+            person2.adress=new adress();
+            person2.adress.street = "kaula";
+            person2.adress.city = "sephoy";
+            
+            person person3 = new person();
             person3.name = "mordret";
             person3.age = 27;
-            person3.street = "svaoi";
-            person3.city = "osaka";
+            person3.adress=new adress();
+            person3.adress.street = "raven";
+            person3.adress.city = "BARCELONA";
+           
             person1.Introduce();
             person2.Introduce();
             person3.Introduce();
