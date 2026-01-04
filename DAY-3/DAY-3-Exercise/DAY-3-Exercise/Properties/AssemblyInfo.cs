1d48b4ce2ef0a9a32def307db310b539b1e226fe
Kshitij1ghk <kshitijwk@gmail.com>
2026-01-04 19:10:04 +0530

    - implemented validation,constructors,encapsulation,properties vs fields and access modifiers basics in c#

---
+++ DAY-3/DAY-3-Exercise/DAY-3-Exercise/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DAY-3-Exercise")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DAY-3-Exercise")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c4008945-0c51-49cd-93f6-c027eb5f0ba5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DAY-3/DAY-3-PRACTICE/DAY-3-PRACTICE/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace DAY_3_PRACTICE
+{
+    class car
+    {
+        public string model;
+
+        public car()// using constructor
+        {
+            model = "mustang";
+
+        }
+    }
+    class car1 //using parametrized constructor and multiples
+    {
+        public string model;
+        public string color;
+        public int year;
+        public car1(string model, string color, int year)
+        {
+            if(string.IsNullOrWhiteSpace(model)|| string.IsNullOrWhiteSpace(color)){
+                throw new ArgumentException("name cannot be empty"); //use of validation in constructors
+            }
+            this.model = model;
+            this.color = color;
+            this.year = year;
+        }
+        
+    }
+    class person
+    {
+        private string name; //hidden data
+        public string Name //controlled access
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+    public class Bank
+    {
+        //hiding class data by declaring the variable as private
+        private double balance;
+        //creating pblic setter and getter methods
+        //public getter method is used to return the the data stored in balance variable or field
+        public double getbalance()
+        {
+            return balance;
+        }
+        //public setter method is used to set data in balance variable
+        public void setbalance(double balance)
+        {
+            if (balance >= 0)
+            {
+                this.balance = balance;
+            }
+            else
+            {
+                throw new Exception("please pass a positive value");//generic exception unlike argument exception
+                // used for general errors when doesn't meet specific types 
+            }
+
+            
+        }
+        
+    }
+   //implementing data encapusulation using c#properties
+    class bank
+    {
+        private double amount;
+        public double Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                // validate the vale before storing it 
+                if (value < 0)
+                {
+                    throw new Exception("please pass a positive value");
+                }
+                else
+                {
+                    amount=value;
+                }
+            }
+        }
+    }
+    //understanding static vs nonstatic
+    class Example
+    {
+        int x; //Non static variable //x belongs to each object
+        static int y = 200; //y belongs to the class itsel
+        public Example(int x)
+        {
+            this.x = x;
+        }
+    }
+
+    internal class Program
+        {
+        static void Main(string[] args)
+        {
+
+            car ford = new car();
+            Console.WriteLine(ford.model);
+            car1 mercedes = new car1("benz", "blue", 5);
+            car1 ford1 = new car1("mustang", "red", 56);
+            Console.WriteLine(mercedes.model + mercedes.color + mercedes.year);
+            person person1=new person();
+            person1.Name = "liam";
+            Console.WriteLine(person1.Name);
+            Bank bank=new Bank();
+            // you cannot access the private ariable 
+            //bank.balance will give compile time eroor
+            // you can access private varaible via public setter and getter
+            bank.setbalance(500);
+            Console.WriteLine(bank.getbalance());
+            //Bank bank1=new Bank();
+            //bank1.setbalance(-200); gives an exception error due to -ve balance
+            bank Bank = new bank();
+            //we cannot access the amount variable directly
+            //Bank.amout gives error since we are using properties for encapsulation
+            //that is why we use public Amount property
+            Bank.Amount = 1000;
+            Console.WriteLine(Bank.Amount);
+            //if we wrote negative it gives an error
+            }
+        }
+    
+}
