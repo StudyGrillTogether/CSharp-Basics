73861347a7e256c7488538df17743722b02ea44f
Kshitij1ghk <kshitijwk@gmail.com>
2026-01-08 04:56:23 +0530

    create a interface Ilogger with Consolelogger use logger inside another class

---
+++ DAY-6/DAY-6-EXERCISE-task/DAY-6-EXERCISE-task/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_6_EXERCISE_task
+{
+    interface Ilogger
+    {
+        void Consolelogger();
+    }
+    public class implemntIloger : Ilogger
+    {
+        public void Consolelogger()
+        {
+            Console.WriteLine("calling interface abstract methods inside a class ");
+        }
+    }
+
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
