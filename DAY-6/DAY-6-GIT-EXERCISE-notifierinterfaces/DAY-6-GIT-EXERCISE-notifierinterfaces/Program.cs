8e0323e7e398122cbc57e440a14a2515b3f73430
Kshitij1ghk <kshitijwk@gmail.com>
2026-01-08 05:40:28 +0530

    - implemnted some tasks of git exercise

---
+++ DAY-6/DAY-6-GIT-EXERCISE-notifierinterfaces/DAY-6-GIT-EXERCISE-notifierinterfaces/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_6_GIT_EXERCISE_notifierinterfaces
+{
+    interface Inotifier
+    {
+        void send(string message);
+    }
+    public class consolenotifier : Inotifier
+    {
+        public void send(string message)
+        {
+            Console.WriteLine("console notification"+message);
+        }
+    }
+    public class emailnotifier : Inotifier
+    {
+        public void send(string message)
+        {
+            Console.WriteLine("email notificiation"+message); 
+        }
+    }
+    public class orderservice
+    {
+        
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inotifier console=new consolenotifier();
+            Inotifier email=new emailnotifier();
+            console.send("kshitij");
+            email.send("email reciverd");
+        }
+    }
+}
